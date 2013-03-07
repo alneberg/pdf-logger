0bfce6af76742a6772d43eafbff97a3f69a0ed46
Johannes Alneberg <alneberg@kth.se>
2013-03-07 14:43:39 +0100

    Tests for init

--- PdfLogger/tests/test_core.py
+++ PdfLogger/tests/test_core.py
@@ -4,20 +4,41 @@ from nose.tools import assert_almost_equal, assert_equal,\
     assert_is_none
 import os
 import tempfile
+import shutil
 
 from pdflogger.core import init
+from pdflogger.parser import main_parser
 
 class TestCore(object):
+    def __init__(self):
+        self.tmp_dir = None
+
     def setUp(self):
-        pass
+        cur_dir = os.path.dirname(os.path.realpath(__file__))
+        self.tmp_dir =  os.path.join(cur_dir,'tmp')
+
     def tearDown(self):
-        pass
+        log_dir = self.tmp_dir =  os.path.join(self.tmp_dir,'log')
+        shutil.rmtree(log_dir)
+        
     # testing function: init
-    def test_init(self):
-        with tempfile.NamedTemporaryFile() as tmp_file:
-            sg.print_group_contigs(tmp_file)
-            tmp_file.seek(0)
-            contig_seqs = list(SeqIO.parse(tmp_file, "fasta"))
-            assert_equal(len(contig_seqs),10)
-            assert_equal(contig_seqs[0].id, "Capnocytophaga_canimorsus_Cc5_uid70727_10")
+    def test_init_creates_log_directory(self):
+        tmp_dir = self.tmp_dir
+        parser = main_parser()
+        args = parser.parse_args(['init','my_temp_log_file', '--author=testing_author'])
+        args.dir = tmp_dir
+        init(args)
+        tmp_dir_list = os.listdir(tmp_dir)
+        assert_equal(("log" in tmp_dir_list),True)
+
+    def test_init_creates_log_file(self):
+        tmp_dir = self.tmp_dir
+        parser = main_parser()
+        args = parser.parse_args(['init','my_temp_log_file', '--author=testing_author'])
+        args.dir = tmp_dir
+        init(args)
+        log_dir = os.path.join(tmp_dir,'log')
+        log_dir_list = os.listdir(log_dir)
+        assert_equal(("my_temp_log_file_log.tex" in log_dir_list),True)
+        
 
