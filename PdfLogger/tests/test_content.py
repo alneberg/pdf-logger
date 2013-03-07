9437639c4013e5477d07d2a32741345a7ce1c74a
Johannes Alneberg <alneberg@kth.se>
2013-03-07 16:35:24 +0100

    Failing test on correct section insertion

--- PdfLogger/tests/test_content.py
+++ PdfLogger/tests/test_content.py
@@ -5,9 +5,11 @@ from nose.tools import assert_almost_equal, assert_equal,\
 import os
 import tempfile
 import sys
+import re
 
 from pdflogger.parser import main_parser
 from pdflogger.content import section, subsection, add_content
+from pdflogger.core import latex_template
 
 class TestContent(object):
     def setUp(self):
@@ -15,7 +17,7 @@ class TestContent(object):
     def tearDown(self):
         pass
     # testing function: main
-    def test_section(self):
+    def test_section_can_write(self):
 
         parser = main_parser()
         args = parser.parse_args(['section','temp_header'])
@@ -30,7 +32,24 @@ class TestContent(object):
                 true_first_line = r"\section{temp_header}"
                 assert_equal(true_first_line, first_line)
 
-    def test_subsection(self):
+    def test_section_can_insert_correctly(self):
+        parser = main_parser()
+        args = parser.parse_args(['section','temp_header'])
+
+        with tempfile.NamedTemporaryFile(suffix='_log.tex') as tmp_file:
+            di = '\tmp'
+            with open(os.path.join(di,tmp_file.name),'r+') as f:
+                f.write(latex_template("my_title","my_author"))
+                f.seek(0)
+                args.log_file = f
+                section(args)
+                f.seek(0)
+                tex_string = f.read()
+                right_order = re.match(".+maketitle.+section.+document",tex_string)
+                
+                assert_equal(right_order, True)
+
+    def test_subsection_can_write(self):
 
         parser = main_parser()
         args = parser.parse_args(['subsection','temp_header'])
